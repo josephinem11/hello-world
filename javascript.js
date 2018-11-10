ad597d3d98a45c748dbfa3406da808c0a433faf1
Josephine Man <josephine.man0112@yahoo.com>
2018-11-10 19:39:37 +0000

    Finished Javascript carousel and added font

--- javascript.js
+++ javascript.js
@@ -1,22 +1,15 @@
-var slidesIndex=0;
+var slideIndex = 0;
 showSlides();
-
 function showSlides() {
-    
-    var slides= document.getElementsByClassName("myimages");
+
+	var slides = document.getElementsByClassName("myimages");
+	for (var i = 0; i < slides.length; i++){
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex >= slides.length){
+		slideIndex = 0;
+	}
+	slides[slideIndex].style.display = "block";
+	setTimeout(showSlides, 4000);
 }
-
-function showSlides () {
-
-    var slides = document.getElementsByClassName("myimages");
-    for (var i=0; i<slides.length; i++){
-        slides [i].style.display= "none"; 
-    }
-    slidesIndex++;
-    if (slidesIndex >= slides.length){
-        slidesIndex=0
-    }
-    slides[slidesIndex].style.display="block"
-    setTimeout(showSlides, 4000);
-    }
-    
